f536d07e76d244a87cdf21775a71edbc95278f8c
Gary Cohen <coheng@users.noreply.github.com>
2018-03-02 09:36:58 +0200

    updated php mail

--- DigiQuill/public/mail.php
+++ DigiQuill/public/mail.php
@@ -7,7 +7,7 @@ $msg 		= $_POST['msg'];
 $subject = 'DigiQuill Website Enquiry';
 $output 	= "Name: ".$fname."\n\nEmail: ".$email."\n\nMessage: ".$msg;
 
-$to 		= 'gary.cohen@pipware.net';
+$to 		= 'kmanana@cyestcorp.com';
 $headers	= 'FROM: "'.$email.'"';
 
 $send		= mail($to, $subject, $output."\n\n***This message has been sent", $headers);
